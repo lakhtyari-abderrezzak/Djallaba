569381be2eda5e81eae1b1be7ac3b15332e2c22c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-01 23:24:43 +0000

    added ForeignId to products

--- database/migrations/2025_01_01_011130_create_products_table.php
+++ database/migrations/2025_01_01_011130_create_products_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description');
             $table->decimal('price', 8, 2);
-            $table->string('category');
             $table->integer('stock');
             $table->string('image_url')->nullable();
             $table->timestamps();
