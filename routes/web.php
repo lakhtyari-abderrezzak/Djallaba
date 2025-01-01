156ced829e5f3c654bf8f3eaa673ff1cd8d66ee8
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-01 18:07:22 +0000

    Add Product resource route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('categories', CategoryController::class);
+
+Route::resource('products', ProductController::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
