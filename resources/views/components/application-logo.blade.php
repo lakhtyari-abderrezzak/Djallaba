ba6af2b2711bb97d56e2ced60512e3727268b486
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-01 18:08:16 +0000

    Configration of log

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center items-center space-x-2">
-    <img style="width: 40px; color: red" src="{{asset('img/logo/hanger-2-svgrepo-com.svg')}}" alt="">
-    <h1 class="text-2xl">Djja<span style="color : #1A76D1">llaba</span></h1>
+<div class="app-logo">
+    <img  src="{{asset('img/logo/hanger-2-svgrepo-com.svg')}}" alt="">
+    <h1 class="text-2xl">Djja<span>llaba</span></h1>
 </div>
