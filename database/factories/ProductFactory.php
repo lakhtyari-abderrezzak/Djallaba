40fbd99cbe06b4cdc63ee1c1b4939219f5a926a6
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-01 23:26:30 +0000

    factories and seeders

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,14 +17,13 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
+            'category_id' => $this->faker->numberBetween(1, 4),
             'name' => $this->faker->word(),
             'slug' => str::slug($this->faker->word()),
             'description' => $this->faker->sentence(),
             'price' => $this->faker->randomFloat(2, 5, 100),
-            'category' => $this->faker->word(),
             'stock' => $this->faker->numberBetween(1, 100),
             'image_url' => $this->faker->imageUrl(),
-
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(4)->create();
         Product::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
